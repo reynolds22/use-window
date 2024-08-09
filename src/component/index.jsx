f7085ada5cbf533d8b04d29fcd2c478a7a304946
reynolds22 <zacharysreynolds1830@gmail.com>
2024-08-09 19:08:08 -0400

    getting back into it

---
+++ src/component/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UseWindow(){
+    return (
+        <div className="container">
+            <h1>Lets Goooo!</h1>
+        </div>
+    );
+};
+
+export default UseWindow;
